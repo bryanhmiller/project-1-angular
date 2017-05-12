c2e2cc62ea0cf28a44c84762ab6dda14b4007f69
bryanhmiller <bryankalos@hotmail.com>
2017-05-11 20:48:05 -0500

    Completed skeleton, currently not functioning

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log("I am here.");
+var app = angular.module("Zoe-favorite-food");
+
+app.controller("ImageCtrl", ($scope) => {
+	$scope.showButtonView = true;
+
+	$scope.button = () => {
+		$scope.showButtonView = false;
+	};
+	$scope.image = () => {
+		$scope.showButtonView = true;
+	};
+});
